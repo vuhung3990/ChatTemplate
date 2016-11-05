8904ed675e90c98c45ed794ad8bcbe8607b70458
vuhung3990 <vuhung3990@gmail.com>
2016-11-05 18:08:55 +0700

    add history page

--- src/pages/history/history.ts
+++ src/pages/history/history.ts
@@ -12,7 +12,43 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'history.html'
 })
 export class History {
-
+  historyConversations: any[] = [
+    {
+      avatar: "https://pbs.twimg.com/profile_images/378800000333141325/c781fd523beb5093b744c3cb1224cf37_400x400.png",
+      name: "Khaleesi",
+      isRead: false,
+      message: "Hash yer dothrae chek?",
+      dateTime: "3:43 pm"
+    },
+    {
+      avatar: "https://pbs.twimg.com/profile_images/600707945911844864/MNogF757_400x400.jpg",
+      name: "Ned Stark",
+      isRead: false,
+      message: "hi, i don't understand Dothraki language",
+      dateTime: "1:43 am"
+    },
+    {
+      avatar: "https://pbs.twimg.com/profile_images/664286819333246982/Vv7k2UN4_400x400.jpg",
+      name: "Jon Snow",
+      isRead: true,
+      message: "do you want to hunt with me?",
+      dateTime: "3:43 pm"
+    },
+    {
+      avatar: "https://pbs.twimg.com/profile_images/637660458187649024/u90FeDjX.jpg",
+      name: "Varys (King Landing)",
+      isRead: false,
+      message: "have a good news for you, call me now",
+      dateTime: "yesterday"
+    },
+    {
+      avatar: "https://pbs.twimg.com/profile_images/703652594586152960/CuijbaHC.jpg",
+      name: "Jorah Mormont",
+      isRead: true,
+      message: "can i take care of your wife before you die?",
+      dateTime: "sep 15"
+    }
+  ] 
   constructor(public navCtrl: NavController) {}
 
   ionViewDidLoad() {
